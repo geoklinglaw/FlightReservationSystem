102069ebb45cc58c933999f14e1665eef3e65d8a
geokling <geoklinglaw@gmail.com>
2023-11-13 04:27:50 +0800

    view all FSP details

--- FRSManagementClient/src/java/frsmanagementclient/ScheduleManagerTask.java
+++ FRSManagementClient/src/java/frsmanagementclient/ScheduleManagerTask.java
@@ -62,8 +62,8 @@ public class ScheduleManagerTask {
             System.out.println("4: Update flight");
             System.out.println("5: Delete flight");
             System.out.println("6: Create Flight Schedule Plan");
-            System.out.println("7: View all Flight Plans");
-            System.out.println("8: View all Flight Plan Details");
+            System.out.println("7: View all Flight Schedule Plans");
+            System.out.println("8: View all Flight Schedule Plan Details");
             System.out.println("9: Update Flight Schedule Plan");
             System.out.println("10: Delete Flight Schedule Plan");
 
@@ -94,7 +94,11 @@ public class ScheduleManagerTask {
                 } 
                 else if (response == 7) {
                     viewFlightSchedulePlan();
-                } else {
+                } 
+                else if (response == 8){
+                    viewFlightSchedulePlanDetails(scanner);
+                } 
+                else {
                     System.out.println("Invalid option, please try again!\n");                
                 }
             }
@@ -540,7 +544,7 @@ public class ScheduleManagerTask {
         FRSManagementSessionBeanRemote.createFlightScheduleAndPlan(fsList, fsp, flight);
     }
     
-    private void viewFlightSchedulePlan() {
+    private List<FlightSchedulePlan> viewFlightSchedulePlan() {
         System.out.println("\n\n*** View All Flight Schedule Plan *** \n");
         List<FlightSchedulePlan> fspList = FRSManagementSessionBeanRemote.viewAllFlightSchedulePlan();
 
@@ -560,5 +564,37 @@ public class ScheduleManagerTask {
         }
         
         System.out.print(fspText);
+        return fspList;
+    }
+    
+    private void viewFlightSchedulePlanDetails(Scanner sc) {
+        List<FlightSchedulePlan> fspList = viewFlightSchedulePlan();
+        System.out.print("Select which flight schedule plan details you would like to know:\n> ");
+        int response = sc.nextInt();
+        FlightSchedulePlan fsp = fspList.get(response - 1);
+        
+        String fspDetails = "-- Flight Schedule Plan Details -- \n";
+        fspDetails += "Flight Number: " + fsp.getFlight().getFlightNumber() + "\n";
+        if (fsp instanceof SinglePlan) {
+            fspDetails += "Plan Type: Single Plan \n";
+        } else if (fsp instanceof MultiplePlan) {
+            fspDetails += "Plan Type: Multiple Plan \n";
+        } else if (fsp instanceof MultiplePlan) {
+            fspDetails += "Plan Type: Recurrent N Plan \n";
+        } else {
+            fspDetails += "Plan Type: Recurrent Weekly Plan \n";
+        }
+        
+        fspDetails += "| List of flight schedules | \n";
+        int index = 1;
+        List<FlightSchedule> sortedFS = fsp.getFlightSchedule();
+        sortedFS.sort((fs1, fs2) -> fs1.getDepartureTime().compareTo(fs2.getDepartureTime()));
+
+        for (FlightSchedule fs: sortedFS) {
+            fspDetails += index + ": " + fs.getDepartureTime() + " --> " + fs.getArrivalTime() + "\n";
+            index += 1;
+        }
+        
+        System.out.println(fspDetails);
     }
 }
